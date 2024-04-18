3f58376cd6fe3acc5eea50816c1ebdcd93324c78
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-04-18 14:35:21 -0400

    Added a view model to manage state within the app, and handle CRUD (create, read, update, delete) operations.

---
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  TodoListViewModel.swift
+//  TodoList
+//
+//  Created by 宮川義之助 on 2024/04/18.
+//
+
+import Foundation
+
+@Observable
+class TodoListViewModel {
+    
+    // MARK: Stored properties
+    // The list of to-do items
+    var todos: [TodoItem]
+    
+    // MARK: Initializer(s)
+    init(todos: [TodoItem] = []) {
+        self.todos = todos
+    }
+    
+    // MARK: Functions
+    func createToDo(withTitle title: String) {
+        
+        // Create the new to-do item instance
+        let todo = TodoItem(
+            title: title,
+            done: false
+        )
+        
+        // Append to the array
+        todos.append(todo)
+        
+    }
+    
+    func delete(_ todo: TodoItem) {
+        
+        // Remove the provided to-do item from the array
+        todos.removeAll { currentItem in
+            currentItem.id == todo.id
+        }
+        
+    }
+    
+}
