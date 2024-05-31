6c7400b6b078194f7afcd65729998227b8156a35
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-05-31 11:32:28 -0400

    Added a new data structure to represent an image that was picked (selected) from the photo library of the device.

---
+++ TodoList/Model/TodoItemImage.swift
@@ -0,0 +1,62 @@
+//
+//  TodoItemImage.swift
+//  TodoList
+//
+//  Created by 宮川義之助 on 2024/05/31.
+//
+
+import SwiftUI
+
+struct TodoItemImage: Transferable, Equatable {
+    
+    // MARK: Stored properties
+    let image: Image
+    let data: Data
+    
+    // MARK: Computed properties
+    
+    // Required to conform to Transferable protocol
+    // Is invoked when user picks (attempts to import) an image from photo library into this app
+    static var transferRepresentation: some TransferRepresentation {
+        
+        return DataRepresentation(importedContentType: .image) { importedImageData in
+            
+            // Attempt to create an instance of TodoItemImage from the imported data
+            guard let image = TodoItemImage(rawImageData: importedImageData) else {
+                // If the important did not work, throw an error
+                throw TransferError.importFailed
+            }
+            
+            // The import worked, so return the imported image
+            return image
+        }
+    }
+}
+
+// Extend the structure to add new capabilities
+extension TodoItemImage {
+    
+    // MARK: Initializer(s)
+    
+    // An initializer to create an instance of TodoItemImage from the image data
+    // returned by the PhotosPicker
+    init?(rawImageData: Data) {
+        
+        // Create an instance of UIImage from the raw image data provided
+        guard let uiImage = UIImage(data: rawImageData) else {
+            return nil
+        }
+        
+        // Create a SwiftUI Image structure from the UIImage instance
+        let image = Image(uiImage: uiImage)
+        
+        // Initialize and return TodoImage instance
+        self.init(image: image, data: rawImageData)
+    }
+    
+}
+
+// List the possible errors that can occur when importing an image
+enum TransferError: Error {
+    case importFailed
+}
