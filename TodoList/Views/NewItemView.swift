c7e1e58816941767ea9a2f36aa90065f1bae03f9
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-05-31 11:40:01 -0400

    Can now select and load a photo from the Photos library.

--- TodoList/Views/NewItemView.swift
+++ TodoList/Views/NewItemView.swift
@@ -5,6 +5,7 @@
 //  Created by 宮川義之助 on 2024/05/30.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct NewItemView: View {
@@ -13,6 +14,12 @@ struct NewItemView: View {
     
     // The item currently being added
     @State var newItemDescription = ""
+    
+    // The selection made in the PhotosPicker
+    @State var selectionResult: PhotosPickerItem?
+
+    // The actual image loaded from the selection that was made
+    @State var newItemImage: TodoItemImage?
     
     // Access the view model through the environment
     @Environment(TodoListViewModel.self) var viewModel
@@ -32,12 +39,40 @@ struct NewItemView: View {
                         viewModel.createToDo(withTitle: newItemDescription)
                         // Clear the input field
                         newItemDescription = ""
+                        // Clear the photo picker selection result
+                        selectionResult = nil
+                        // Clear the loaded photo
+                        newItemImage = nil
                     }
                     .font(.caption)
                     .disabled(newItemDescription.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty == true)
                 }
 
-                Spacer()
+                HStack {
+                    
+                    PhotosPicker(selection: $selectionResult, matching: .images) {
+                        
+                        // Has an image been loaded?
+                        if let newItemImage = newItemImage {
+                            
+                            // Yes, show it
+                            newItemImage.image
+                                .resizable()
+                                .scaledToFit()
+
+                        } else {
+                            
+                            // No, show an icon instead
+                            Image(systemName: "photo.badge.plus")
+                                .symbolRenderingMode(.multicolor)
+                                .font(.system(size: 30))
+                                .foregroundStyle(.tint)
+                            
+                        }
+                    }
+
+                }
+                .frame(height: 100)
             }
             .padding(20)
             .toolbar {
@@ -51,10 +86,38 @@ struct NewItemView: View {
                     
                 }
             }
+            // This block of code is invoked whenever the selection from the picker changes
+            .onChange(of: selectionResult) {
+                // When the selection result is not nil...
+                if let imageSelection = selectionResult {
+                    // ... transfer the data from the selection result into
+                    // an actual instance of TodoItemImage
+                    loadTransferable(from: imageSelection)
+                }
+            }
+
+            
+            
         }
 
 
     }
+    
+    // MARK: Functions
+
+    // Transfer the data from the PhotosPicker selection result into the stored property that
+    // will hold the actual image for the new to-do item
+    private func loadTransferable(from imageSelection: PhotosPickerItem) {
+        Task {
+            do {
+                // Attempt to set the stored property that holds the image data
+                newItemImage = try await imageSelection.loadTransferable(type: TodoItemImage.self)
+            } catch {
+                debugPrint(error)
+            }
+        }
+    }
+    
 }
 
 #Preview {
